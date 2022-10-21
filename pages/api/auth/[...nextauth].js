8e073cca12ea9e0e110ea17829af89b3b7fe1b75
humphrey-mutuma <humphreymutuma01@gmail.com>
2022-10-21 20:10:22 +0300

    setup nextauth google login API route

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,28 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+ import clientPromise from "../../../backend/config/mongodb";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+
+export const authOptions = {
+  /* use the MongoDB adapter, initiate a
+  cached MongoDB connection in /config/mongodb.js
+  and pass the connection to the adapter
+  */
+  adapter: MongoDBAdapter(clientPromise),
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    
+    // ... more providers here
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "jwt",
+  },
+  debug: false,
+};
+
+export default NextAuth(authOptions);
