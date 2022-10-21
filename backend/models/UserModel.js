accd62fa869cae5cf406158a98aa8653281bfec4
humphrey-mutuma <humphreymutuma01@gmail.com>
2022-10-21 19:53:25 +0300

    create a user model, mongodb connection for next-auth mongodb adapter login and  mongoose ODM connection

---
+++ backend/models/UserModel.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, "Please provide user name."],
+     },
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true, // add automatic timestamps
+  }
+);
+
+export default mongoose.models.User || mongoose.model("User", UserSchema);
