c9a5608331be80ec50fa3cb37ea697da933fe550
humphrey-mutuma <humphreymutuma01@gmail.com>
2022-10-21 20:03:51 +0300

    update next.config to add images allowed domains, and formats

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: [
+      "lh3.googleusercontent.com", //from  google avatar image
+    ],
+    formats: ["image/avif", "image/webp"], //minify images according to browser support
+  },
+};
 
 module.exports = nextConfig
